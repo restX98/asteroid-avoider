422564a9780fad299daad64869466497034ba9be
Enrico Restuccia <enrico.rest98@gmail.com>
2025-02-09 18:29:59 +0000

    Client: reworked app ans solar system and added background environment

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,36 +1,11 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import SolarSystem from "@/components/SolarSystem";
-import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
-
-const MyMesh = () => {
-  const controlsRef = useRef();
-  const planetRef = useRef();
-
-  // TODO: Remove to manage it on planet click
-  useFrame(() => {
-    if (planetRef.current && controlsRef.current) {
-      controlsRef.current.target.copy(planetRef.current.position);
-      controlsRef.current.update();
-    }
-  });
-
-  return (
-    <mesh>
-      <ambientLight intensity={0.2} />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
-      <SolarSystem planetRef={null} />
-      <OrbitControls ref={controlsRef} />
-    </mesh>
-  );
-};
 
 function App() {
   return (
     <div id="canvas-container" style={{ width: "100vw", height: "100vh" }}>
       <Canvas camera={{ position: [0, 0, 2100], far: 10000000 }}>
-        <MyMesh />
+        <SolarSystem />
       </Canvas>
     </div>
   );
--- packages/client/src/components/SolarSystem.jsx
+++ packages/client/src/components/SolarSystem.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import Planet from "@/components/Planet";
 import Trajectory from "@/utils/Trajectory";
+import { useFrame } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
 
 import planetData from "@/data/planets.json";
 import { SCALE_FACTOR } from "@/data/config.json";
 
-function SolarSystem({ planetRef }) {
+function SolarSystem() {
+  const controlsRef = useRef();
+  const planetRef = useRef();
+
+  // TODO: Remove to manage it on planet click
+  useFrame(() => {
+    if (planetRef.current && controlsRef.current) {
+      controlsRef.current.target.copy(planetRef.current.position);
+      controlsRef.current.update();
+    }
+  });
+
   const planets = Object.keys(planetData).map((planetKey) => {
     const data = planetData[planetKey];
     const trajectory = new Trajectory(
@@ -28,6 +41,17 @@ function SolarSystem({ planetRef }) {
 
   return (
     <>
+      <Environment
+        files="/textures/starmap_g8k.jpg"
+        background
+        backgroundBlurriness={0}
+      />
+      <mesh>
+        <ambientLight intensity={0.2} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
+        <OrbitControls ref={controlsRef} />
+      </mesh>
+
       {/* Sun */}
       <mesh>
         <sphereGeometry args={[0.004655 * SCALE_FACTOR, 32, 32]} />
@@ -44,7 +68,7 @@ function SolarSystem({ planetRef }) {
           trajectory={trajectory}
           color={color}
           radius={radius}
-          followRef={planetRef}
+          followRef={null}
         />
       ))}
     </>
