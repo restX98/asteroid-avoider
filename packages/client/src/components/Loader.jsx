9b7b6637e7859a7b981144829b0e3c1f50355a36
Enrico Restuccia <enrico.rest98@gmail.com>
2025-02-19 20:37:50 +0000

    Client: Update loader

--- packages/client/src/components/Loader.jsx
+++ packages/client/src/components/Loader.jsx
@@ -2,10 +2,27 @@ import { useEffect, Suspense } from "react";
 import { useProgress, Html } from "@react-three/drei";
 
 import { Progress } from "@/components/ui/progress";
+import { AsteroidIcon } from "@/components/icons/AsteroidIcon";
 
 const ProgressBar = ({ progress }) => {
   return (
-    <Html fullscreen={true} className="flex justify-center items-center">
+    <Html
+      fullscreen={true}
+      className="flex flex-col justify-center items-center gap-y-5"
+    >
+      <div className="flex gap-x-3 text-4xl">
+        <AsteroidIcon
+          className="h-10"
+          fillColor="#4A4A4A"
+          strokeColor="black"
+        />
+        <span>ASTEROID AVOIDER</span>
+        <AsteroidIcon
+          className="h-10"
+          fillColor="#4A4A4A"
+          strokeColor="black"
+        />
+      </div>
       <Progress value={progress} className="lg:w-1/3 md:w-2/3 w-4/5" />
     </Html>
   );
