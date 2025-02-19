9a8c53490deb9e61a37847181103aa80bb1cff5c
Enrico Restuccia <enrico.rest98@gmail.com>
2025-02-19 20:58:55 +0000

    remove reminder comment

--- packages/server/routes/asteroids.js
+++ packages/server/routes/asteroids.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-// Import the controller that will handle the logic
 const { getAsteroids } = require("../controllers/asteroids");
 const { getAsteroidDetail } = require("../controllers/asteroidDetails");
 
-// Define a GET route to fetch asteroids information
 router.get("/", getAsteroids);
 
 router.get("/:asteroidId", getAsteroidDetail);
